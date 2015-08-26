03cde03acca46085751195de9bca1c0017ab6e18
p10614040 <695795023@qq.com>
2015-08-26 17:04:58 +0800

    读取当前路径

--- src/com/corejava9/chapter/io/StreamMainTest.java
+++ src/com/corejava9/chapter/io/StreamMainTest.java
@@ -9,6 +9,12 @@
  */
 package com.corejava9.chapter.io;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javax.imageio.stream.FileImageInputStream;
+
 /**
  * @author YanLiang
  *
@@ -17,16 +23,20 @@ public class StreamMainTest {
 
 	/**
 	 * @author yan.liang
+	 * @throws FileNotFoundException 
 	 * @date 2015年7月21日 下午4:05:28
 	 * @Description TODO(这里用一句话描述这个方法的作用)
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// 运行目录
 		System.out.println(System.getProperty("user.dir"));
 		// 运行环境分割字符
 		System.out.println(java.io.File.separator);
 		// 换行
 		System.out.println(System.getProperty("line.separator"));
+		
+		System.out.println(new File(".").getAbsolutePath());
+		FileInputStream fileInputStream = new FileInputStream("myfile.txt");
 	}
 
 }
